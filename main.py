d2f8ab288188a5fb7f7ea393f8bb1598ac8d5b09
itemic <terrankroft@gmail.com>
2019-04-12 15:40:17 +0530

    Add convert to CSV

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 page = requests.get("http://www.scholarscup.org/calendar/")
 
@@ -7,9 +8,23 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 events = soup.findAll("div", {"class": "event"})
 
+names = []
+dates = []
+
 for event in events:
     details = event.find("div", {"class": "span5"})
 
-    name = [elem.find("a").text for elem in details.findAll("h3")][0]
-    date = event.find("div", {"class": "date"}).text.strip()
+    name = [elem.find("a").text for elem in details.findAll("h3")][0].encode().strip()
+    
+    date = event.find("div", {"class": "date"}).text.encode().strip()
 
+    names.append(name)
+    dates.append(date)
+
+print(names)
+print(dates)
+
+with open('sitecal.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    for n, d in zip(names, dates):
+        writer.writerow([n, d])
